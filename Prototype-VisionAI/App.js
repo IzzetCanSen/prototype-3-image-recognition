f44db53f80023f76a6c4bd7061aa06cb5975bba5
Izzet Can Sen <izzet.sen@student.ehb.be>
2023-11-17 03:15:40 +0100

    Updated the UI &  added loading animation

--- Prototype-VisionAI/App.js
+++ Prototype-VisionAI/App.js
@@ -6,12 +6,13 @@ import {
   Text,
   View,
   TouchableOpacity,
-  Alert,
   Image,
+  ActivityIndicator,
 } from "react-native";
 import { Camera } from "expo-camera";
 import * as FileSystem from "expo-file-system";
 import Icon from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import axios from "axios";
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [isCameraVisible, setIsCameraVisible] = useState(false);
   const [labels, setLabels] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const cameraRef = useRef(null);
 
@@ -69,6 +71,9 @@ export default function App() {
         return;
       }
 
+      setIsLoading(true);
+      console.log("Loading started");
+
       const apiKey = API_KEY;
       const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
 
@@ -92,25 +97,21 @@ export default function App() {
     } catch (error) {
       console.error("Error analyzing photo:", error);
       alert("Error analyzing photo. Please try again later.");
+    } finally {
+      setIsLoading(false);
+      console.log("Loading finished");
     }
   };
 
+  const deletePhoto = () => {
+    setPhotoUri(null);
+    setLabels([]);
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={styles.containerr}>
       {isCameraVisible ? (
         <View style={styles.cameraWrapper}>
-          <View style={styles.cameraUiCloseContainer}>
-            <TouchableOpacity
-              style={styles.closeCameraButton}
-              onPress={toggleCamera}
-            >
-              <Icon
-                style={styles.closeCameraButtonIcon}
-                name="arrow-left"
-                size={28}
-              />
-            </TouchableOpacity>
-          </View>
           <Camera
             style={styles.camera}
             type={type}
@@ -127,27 +128,47 @@ export default function App() {
           </View>
         </View>
       ) : (
-        <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCamera}>
-            <Icon style={styles.buttonText} name="camera" size={32} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={analyzePhoto}>
-            <Icon style={styles.buttonText} name="search" size={32} />
-          </TouchableOpacity>
+        <View style={styles.mainContainer}>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCamera}>
+              <Icon style={styles.iconButton} name="camera" size={22} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={analyzePhoto}>
+              <MaterialCommunityIcons
+                style={styles.iconButton}
+                name="face-recognition"
+                size={22}
+              />
+            </TouchableOpacity>
+          </View>
           <View>
             {photoUri && (
-              <Image
-                source={{ uri: photoUri }}
-                style={{ width: 300, height: 300, marginBottom: 20 }}
-              />
+              <View>
+                <Image source={{ uri: photoUri }} style={styles.image} />
+                {isLoading && (
+                  <View style={styles.loadingOverlay}>
+                    <ActivityIndicator size="large" color="#FAF9FB" />
+                  </View>
+                )}
+                <TouchableOpacity
+                  style={styles.deletePhotoButtonContainer}
+                  onPress={deletePhoto}
+                >
+                  <MaterialCommunityIcons
+                    style={styles.deletePhotoButton}
+                    name="delete"
+                    size={24}
+                  />
+                </TouchableOpacity>
+              </View>
             )}
             {labels.length > 0 && (
-              <View>
-                <Text style={styles.labelHeader}>Labels:</Text>
+              <View style={styles.labelsContainer}>
                 {labels.map((label, index) => (
                   <View key={index} style={styles.labelContainer}>
+                    <Text style={styles.labelText}>{label.description}</Text>
                     <Text style={styles.labelText}>
-                      {label.description}: {label.score.toFixed(2)}
+                      {(label.score * 100).toFixed(0)}%
                     </Text>
                   </View>
                 ))}
@@ -161,6 +182,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  containerr: {
+    flex: 1,
+    backgroundColor: "#FAF9FB",
+    alignItems: "center",
+  },
+
+  mainContainer: {
+    flex: 1,
+    width: "90%",
+    marginTop: 75,
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -203,6 +236,66 @@ const styles = StyleSheet.create({
   },
 
   buttonsContainer: {
-    rowGap: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 15,
+  },
+
+  button: {
+    width: "48%",
+    backgroundColor: "#5B1D8A",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    borderRadius: 15,
+  },
+
+  iconButton: {
+    color: "#FAF9FB",
+  },
+
+  image: {
+    aspectRatio: 3 / 4,
+    borderRadius: 15,
+  },
+
+  deletePhotoButtonContainer: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+  },
+
+  deletePhotoButton: {
+    color: "#FAF9FB",
+  },
+
+  labelsContainer: {
+    rowGap: 10,
+    backgroundColor: "#5B1D8A",
+    marginTop: 15,
+    padding: 15,
+    borderRadius: 15,
+  },
+
+  labelContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#5B1D8A",
+    padding: 10,
+    borderRadius: 15,
+  },
+
+  labelText: {
+    color: "#FAF9FB",
+    fontSize: 14,
+    fontWeight: "700",
+  },
+
+  loadingOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    borderRadius: 15,
   },
 });
